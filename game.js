c26e98952917705b6404c6c2b5c04be153383f4b
Ks Prajwal <ksprajwal35@gmail.com>
2021-04-28 11:08:47 +0530

    game

--- game.js
+++ game.js
@@ -3,6 +3,32 @@
 var p1 = 0
 var p2 = 1
 
+function resetb()
+{
+    
+    if( p1 == 0){
+        console.log("p2 winner");
+        
+        confirm("player 2 winner")
+    }
+    else{
+        console.log("p1 winner");
+        confirm("player 1 winner")
+    }
+    document.getElementById("square_one_text").innerText = ""
+    document.getElementById("square_two_text").innerText = ""
+    document.getElementById("square_three_text").innerText = ""
+    document.getElementById("square_four_text").innerText = ""
+    document.getElementById("square_five_text").innerText = ""
+    document.getElementById("square_six_text").innerText = ""
+    document.getElementById("square_seven_text").innerText = ""
+    document.getElementById("square_eight_text").innerText = ""
+    document.getElementById("square_nine_text").innerText = ""
+    p1 = 0;
+    p2 = 1;
+}
+
+
 function checkt()
 {
     var s1 = document.getElementById("square_one_text").innerText
@@ -19,6 +45,48 @@ function checkt()
     if((s1 == s2 && s2 == s3 && s1 !="") )
     {
         console.log("winner");
+        resetb();
+    }
+
+    if((s4 == s5 && s5 == s6 && s4 !="") )
+    {
+        console.log("winner");
+        resetb();
+    }
+
+    if((s7 == s8 && s8 == s9 && s7 !="") )
+    {
+        console.log("winner");
+        resetb();
+    }
+
+    if((s1 == s4 && s4 == s7 && s1 !="") )
+    {
+        console.log("winner");
+        resetb();
+    }
+
+    if((s2 == s5 && s5 == s8 && s2 !="") )
+    {
+        console.log("winner");
+        resetb();
+    }
+
+    if((s3 == s6 && s6 == s9 && s3 !="") )
+    {
+        console.log("winner");
+        resetb();
+    }
+    if((s1 == s5 && s5 == s9 && s1 !="") )
+    {
+        console.log("winner");
+        resetb();
+    }
+
+    if((s3 == s5 && s5 == s7 && s3 !="") )
+    {
+        console.log("winner");
+        resetb();
     }
 
 
@@ -112,7 +180,7 @@ document.getElementById("square_four").onclick = function() {
             p2 = 0
             console.log("p1");
         }
-        
+        checkt()
         
     }
 }
@@ -136,7 +204,7 @@ document.getElementById("square_five").onclick = function() {
             p2 = 0
             console.log("p1");
         }
-        
+        checkt()
         
     }
 }
@@ -159,7 +227,7 @@ document.getElementById("square_six").onclick = function() {
             p2 = 0
             console.log("p1");
         }
-        
+        checkt()
         
     }
 }
@@ -184,6 +252,7 @@ document.getElementById("square_seven").onclick = function() {
             console.log("p1");
         }
         
+        checkt()
         
     }
 }
@@ -207,7 +276,7 @@ document.getElementById("square_eight").onclick = function() {
             console.log("p1");
         }
         
-        
+        checkt()
     }
 }
 
@@ -231,6 +300,6 @@ document.getElementById("square_nine").onclick = function() {
             console.log("p1");
         }
         
-        
+        checkt()
     }
 }
